ce759b3febe07b409ac70185dd167877584026ca
Peter <pc.dean@icloud.com>
2020-03-24 14:09:35 +0000

    refactor test to remove repotition of subject definition

--- spec/features/deposit_and_withdrawal_spec.rb
+++ spec/features/deposit_and_withdrawal_spec.rb
@@ -3,16 +3,18 @@ require 'account'
 
 describe Client do
 
+  subject(:client) do
+    test = Client.new
+    test.deposit(100)
+    return test
+  end
+
   it 'client.deposit(amount) should increase balance in acount by amount' do
-    subject = Client.new
-    subject.deposit(100)
-    expect { subject.deposit(30) }.to output(/Balance: 130/).to_stdout
+    expect { client.deposit(30) }.to output(/Balance: 130/).to_stdout
   end
 
   it 'client.withdrawal(amount) should decrease balance in acount by amount' do
-    subject = Client.new
-    subject.deposit(100)
-    expect { subject.withdrawal(30) }.to output(/Balance: 70/).to_stdout
+    expect { client.withdrawal(30) }.to output(/Balance: 70/).to_stdout
   end
 
 end
--- spec/features/statement_spec.rb
+++ spec/features/statement_spec.rb
@@ -4,10 +4,14 @@ require 'bank'
 
 describe Client do
 
+  subject do
+    test = Client.new(Account.new(3000))
+    test.withdrawal(500, '14/01/2012')
+    test.deposit(300)
+    return test
+  end
+
   it 'client.statment should return account statment' do
-    subject = Client.new(Account.new(3000))
-    subject.withdrawal(500, '14/01/2012')
-    subject.deposit(300)
     expect { subject.statement }.to output(/2012/).to_stdout
     expect { subject.statement }.to output(/500/).to_stdout
     expect { subject.statement }.to output(/2500/).to_stdout
